cbe7076d0de75fb560b799f856b6360756590af5
Rafael Paes de Almeida <rafaelpaesalmeida4@gmail.com>
2024-10-17 12:42:33 -0300

    meu primeiro projeto

--- app.js
+++ app.js
@@ -1,44 +1,26 @@
-const express    = require('express');
-const exphbs     = require('express-handlebars');
-const app        = express();
-const path       = require('path')
-const db         = require('./db/connection')
+const express = require('express');
+const exphbs = require('express-handlebars').engine
+const app = express();
+const path = require('path');
+const db = require('./db/connection');
 const bodyParser = require('body-parser');
  
 const PORT = 3001;
- 
  
 // Configurando o Handlebars como engine de view
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
  
-// Definindo a rota principal
-app.get('/', (req, res) => {
-    res.render('home'); // Certifique-se de que o arquivo 'home.handlebars' existe
-});
+// Configuração do Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
  
-
-app.listen(PORT, () => {
-    console.log(`O Express está rodando na porta ${PORT}`);
-});
-//body parser 
-app.use(bodyParser.urlencoded({extended: false }));
- 
-//handlebars
+// Definindo a pasta de views
 app.set('views', path.join(__dirname, 'views'));
-app.engine('hanblebars', exphbs.engine({defaultLayout: "main"}));
-app.set('view engine', 'handlebars');
-
-
-
  
- 
-//static folder//
- 
- 
+// Definindo a pasta estática
 app.use(express.static(path.join(__dirname, 'public')));
  
-//db connection
+// Conexão com o banco de dados
 db.authenticate()
   .then(() => {
     console.log('Conectou ao banco de dados com sucesso');
@@ -47,11 +29,15 @@ db.authenticate()
     console.log('Ocorreu um erro ao conectar', err);
   });
  
-//routes
- 
+// Definindo a rota principal
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('home'); // Certifique-se de que o arquivo 'home.handlebars' existe
 });
  
-// jobs routes
+// Jobs routes
 app.use('/jobs', require('./routes/jobs'));
+ 
+// Iniciando o servidor
+app.listen(PORT, () => {
+  console.log(`O Express está rodando na porta ${PORT}`);
+});
